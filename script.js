c59106b51fba44b0bc1a462c0f33bb2f6acd6810
DavidHCCNguyen <creativespecials@gmail.com>
2023-04-08 02:06:04 +0930

    Adding questions and extra stuff

---
+++ script.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Question two: What are HTML tags?",
+        choices: ["a", "b", "c", "d"],
+        answer: "b"
+    },
+    {
+        title: "Question two: What are HTML tags?",
+        choices: ["a", "b", "c", "d"],
+        answer: "b"
+    },
+    {
+        title: "Question two: What are HTML tags?",
+        choices: ["a", "b", "c", "d"],
+        answer: "b"
+    },
+    {
+        title: "Question two: What are HTML tags?",
+        choices: ["a", "b", "c", "d"],
+        answer: "b"
+    },
+    {
+        title: "Question two: What are HTML tags?",
+        choices: ["a", "b", "c", "d"],
+        answer: "b"
+    },
+]
